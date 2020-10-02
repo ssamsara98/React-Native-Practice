0fddf438a05f4546b1cc2fe5bfaff59dc1d6251b
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-10-02 18:52:55 +0700

    The Meals App

--- the-meals-app/App.js
+++ the-meals-app/App.js
@@ -1,8 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+// import { Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
+// import { useScreens } from 'react-native-screens';
+import { enableScreens } from 'react-native-screens';
+
+import MealsNavigator from './navigation/MealsNavigator';
+
+// useScreens();
+enableScreens();
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -18,19 +24,5 @@ export default function App() {
     return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)} />;
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <MealsNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- the-meals-app/screens/CategoryMealsScreen.js
+++ the-meals-app/screens/CategoryMealsScreen.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealList from '../components/MealList';
 
 const CategoryMealScreen = (props) => {
-  return (
-    <View style={styles.screen}>
-      <Text>The Category Meal Screen!</Text>
-    </View>
-  );
+  const catId = props.navigation.getParam('categoryId');
+
+  const displayedMeals = MEALS.filter((meal) => meal.categoryIds.indexOf(catId) >= 0);
+
+  return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
 
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+CategoryMealScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+  };
+};
 
 export default CategoryMealScreen;
