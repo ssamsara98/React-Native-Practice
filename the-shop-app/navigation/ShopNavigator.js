429f39ecfdccce4891b29c17d40409889353b9c0
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-10-03 18:03:25 +0700

    The Shop App

--- the-shop-app/navigation/ShopNavigator.js
+++ the-shop-app/navigation/ShopNavigator.js
@@ -1,27 +1,171 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import CartScreen from '../screens/shop/CartScreen';
+import OrdersScreen from '../screens/shop/OrdersScreen';
+import UserProductsScreen from '../screens/user/UserProductsScreen';
+import EditProductScreen from '../screens/user/EditProductScreen';
 import Colors from '../constants/Colors';
+import HeaderButton from '../components/UI/HeaderButton';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const defaultNavOptions = {
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+  },
+  headerTitleStyle: {
+    fontFamily: 'open-sans-bold',
+  },
+  headerBackTitleStyle: {
+    fontFamily: 'open-sans',
+  },
+  headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+};
 
 function ProductsNavigator() {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
-        },
-        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
-      }}
-    >
+    <Stack.Navigator screenOptions={{ ...defaultNavOptions }}>
       <Stack.Screen
-        name="ProductsOverviewScreen"
+        name="ProductsOverview"
         component={ProductsOverviewScreen}
-        options={{ headerTitle: 'All Products' }}
+        options={(navData) => {
+          return {
+            headerTitle: 'All Products',
+            headerLeft: () => (
+              <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                  title="Menu"
+                  iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
+                  onPress={() => {
+                    navData.navigation.toggleDrawer();
+                  }}
+                />
+              </HeaderButtons>
+            ),
+            headerRight: () => (
+              <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                  title="Cart"
+                  iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+                  onPress={() => {
+                    navData.navigation.navigate('Cart');
+                  }}
+                />
+              </HeaderButtons>
+            ),
+          };
+        }}
+      />
+
+      <Stack.Screen
+        name="ProductDetail"
+        component={ProductDetailScreen}
+        options={(navData) => {
+          return { headerTitle: navData.route.params['productTitle'] };
+        }}
+      />
+
+      <Stack.Screen
+        name="Cart"
+        component={CartScreen}
+        options={() => {
+          return { headerTitle: 'Your Cart' };
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function OrdersNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{ ...defaultNavOptions }}>
+      <Stack.Screen
+        name="Orders"
+        component={OrdersScreen}
+        options={(navData) => {
+          return {
+            headerTitle: 'Your Orders',
+            headerLeft: () => (
+              <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                  title="Menu"
+                  iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
+                  onPress={() => {
+                    navData.navigation.toggleDrawer();
+                  }}
+                />
+              </HeaderButtons>
+            ),
+          };
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function AdminNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{ ...defaultNavOptions }}>
+      <Stack.Screen
+        name="UserProducts"
+        component={UserProductsScreen}
+        options={(navData) => {
+          return {
+            headerTitle: 'Your Products',
+            headerLeft: () => (
+              <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                  title="Menu"
+                  iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
+                  onPress={() => {
+                    navData.navigation.toggleDrawer();
+                  }}
+                />
+              </HeaderButtons>
+            ),
+            headerRight: () => (
+              <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                  title="Add"
+                  iconName={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+                  onPress={() => {
+                    navData.navigation.navigate('EditProduct', {});
+                  }}
+                />
+              </HeaderButtons>
+            ),
+          };
+        }}
+      />
+
+      <Stack.Screen
+        name="EditProduct"
+        component={EditProductScreen}
+        options={(navData) => {
+          const submitFn = navData.route.params['submit'];
+
+          return {
+            headerTitle: navData.route.params['productId'] ? 'Edit Product' : 'Add Product',
+            headerRight: () => (
+              <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                  title="Save"
+                  iconName={Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'}
+                  onPress={submitFn}
+                />
+              </HeaderButtons>
+            ),
+          };
+        }}
       />
     </Stack.Navigator>
   );
@@ -29,10 +173,70 @@ function ProductsNavigator() {
 
 function ShopNavigator() {
   return (
+    <Drawer.Navigator drawerContentOptions={{ activeTintColor: Colors.primary }}>
+      <Drawer.Screen
+        name="Products"
+        component={ProductsNavigator}
+        options={() => {
+          return {
+            drawerIcon: (drawerConfig) => {
+              return (
+                <Ionicons
+                  name={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+                  size={22}
+                  color={drawerConfig.color}
+                />
+              );
+            },
+          };
+        }}
+      />
+
+      <Drawer.Screen
+        name="Orders"
+        component={OrdersNavigator}
+        options={() => {
+          return {
+            drawerIcon: (drawerConfig) => {
+              return (
+                <Ionicons
+                  name={Platform.OS === 'android' ? 'md-list' : 'ios-list'}
+                  size={22}
+                  color={drawerConfig.color}
+                />
+              );
+            },
+          };
+        }}
+      />
+
+      <Drawer.Screen
+        name="Admin"
+        component={AdminNavigator}
+        options={() => {
+          return {
+            drawerIcon: (drawerConfig) => {
+              return (
+                <Ionicons
+                  name={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+                  size={22}
+                  color={drawerConfig.color}
+                />
+              );
+            },
+          };
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
+
+function AppContainer() {
+  return (
     <NavigationContainer>
-      <ProductsNavigator />
+      <ShopNavigator />
     </NavigationContainer>
   );
 }
 
-export default ShopNavigator;
+export default AppContainer;
--- the-shop-app/screens/shop/ProductsOverviewScreen.js
+++ the-shop-app/screens/shop/ProductsOverviewScreen.js
@@ -1,14 +1,51 @@
 import React from 'react';
-import { FlatList, Text } from 'react-native';
-import { useSelector } from 'react-redux';
+import { FlatList, Button, Platform } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 
-const ProductsOverviewScreen = () => {
+import ProductItem from '../../components/shop/ProductItem';
+import * as cartActions from '../../store/actions/cart';
+import Colors from '../../constants/Colors';
+
+const ProductsOverviewScreen = (props) => {
   const products = useSelector((state) => state.products.availableProducts);
+  const dispatch = useDispatch();
+
+  const selectItemHandler = (id, title) => {
+    props.navigation.navigate('ProductDetail', {
+      productId: id,
+      productTitle: title,
+    });
+  };
+
   return (
     <FlatList
       data={products}
       keyExtractor={(item) => item.id}
-      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+      renderItem={(itemData) => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onSelect={() => {
+            selectItemHandler(itemData.item.id, itemData.item.title);
+          }}
+        >
+          <Button
+            color={Colors.primary}
+            title="View Details"
+            onPress={() => {
+              selectItemHandler(itemData.item.id, itemData.item.title);
+            }}
+          />
+          <Button
+            color={Colors.primary}
+            title="To Cart"
+            onPress={() => {
+              dispatch(cartActions.addToCart(itemData.item));
+            }}
+          />
+        </ProductItem>
+      )}
     />
   );
 };
