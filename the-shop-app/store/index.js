5386d5a15042fcd874c90bc807e5b176188723d9
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-10-03 18:03:25 +0700

    The Shop App

--- the-shop-app/store/index.js
+++ the-shop-app/store/index.js
@@ -1,8 +1,13 @@
 import { createStore, combineReducers } from 'redux';
+
 import productsReducer from './reducers/products';
+import cartReducer from './reducers/cart';
+import ordersReducer from './reducers/orders';
 
 const rootReducer = combineReducers({
   products: productsReducer,
+  cart: cartReducer,
+  orders: ordersReducer,
 });
 
 const store = createStore(rootReducer);
